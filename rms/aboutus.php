c2e25e5af42e56a41ca13c8476dae1b696102f27
Hritik1911 <145193362+Hritik1911@users.noreply.github.com>
2024-01-26 15:39:48 +0530

    Add files via upload

---
+++ rms/aboutus.php
@@ -0,0 +1,33 @@
+<?php include('topbar.php');?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/aboutus.css">
+</head>
+<body>
+
+<div class="about-section">
+  <h1> RESULT MANAGEMENT </h1>
+  <p>I have made this student result management project and made all possible efforts over nights to make it hope you like it.</p>
+  <p> Hope this may give me good marks.</p>
+</div>
+
+
+<center><div class="row" >
+     <div class="column">
+          <div class="card">
+               <div class="container">
+                    <h2>HRITIK KUMAR JAISWAL</h2>
+                    <p>Ph No.   :   +91 - 9065959697</p>
+                    <p>hritikjais1911@gmail.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+     </center>
+
+     
+</body>
+</html>
---
+++ rms/contactus.php
@@ -0,0 +1,26 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact Us</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>location - Bhubaneshwar</li>
+         	
+         	<li>Dist - khorda ,Odisha-India</li>
+            <li>Pin - 751024 </li>
+         	<li>contact no - +91 9065959697s
+         	</li>
+					 <li>hritikjais1911@gmail.com</li>
+					 <li><a href= "mailto: hritikjais1911@gmail.com? subject= "> click here to mail us </a>
+         	</li>
+         </ul>
+         <center><h2>Thank you</h2></center>
+    </div>
+</body>
+</html>
